8bda23e0f89340a73985d6e77184b32e85cac609
heini <376428457@qq.com>
2021-01-19 01:04:25 +0800

    00

---
+++ spring-mvc/annotation/src/main/java/com/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+    @RequestMapping("/hello")
+    public String Hello(Model model){
+        model.addAttribute("msg","Hello word");
+        return "hello";
+    }
+}
